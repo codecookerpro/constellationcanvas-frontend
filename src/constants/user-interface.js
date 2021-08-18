bcf425a855da2ada2458c388e43f67c3f3f39d56
unknown <steve.jin.md@gmail.com>
2021-08-19 00:02:29 +0900

    [fix] quickly

--- src/constants/user-interface.js
+++ src/constants/user-interface.js
@@ -10,5 +10,3 @@ export const UI_COLORS = {
   white: '#fff',
 };
 export const FONT_FAMILY = 'sans-serif';
-
-export const AVATAR_COLORS = ['#FFB900', '#D83B01', '#B50E0E', '#E81123', '#B4009E', '#5C2D91', '#0078D7', '#00B4FF', '#008272', '#107C10'];
