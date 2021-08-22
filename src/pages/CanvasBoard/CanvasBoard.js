32c7f42a41354f79fd893c80bcce685d25c133ac
unknown <steve.jin.md@gmail.com>
2021-08-23 06:17:47 +0900

    [fix] scoket.io to receive all incoming changes

--- src/pages/CanvasBoard/CanvasBoard.js
+++ src/pages/CanvasBoard/CanvasBoard.js
@@ -2,11 +2,14 @@ import React, { useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { WidgetEditor } from 'components';
 import { getBoard, setSelectedParticipant } from 'actions';
+import { setupSocket } from 'services/websocket';
+import { removeFigure, setBoard, setCanvasIndex, setFigure } from 'actions';
 
 export default function CanvasBoard() {
   const { index, figures, copiedFigure, selectedParticipant } = useSelector((state) => state.board);
-  const userUUID = useSelector((state) => state.auth.profile.uuid);
   const dispatch = useDispatch();
+  const { accessToken, profile } = useSelector((state) => state.auth);
+  const socket = useMemo(() => accessToken && setupSocket(accessToken), [accessToken]);
   const filteredFigures = useMemo(
     () => figures.filter((f) => f.canvas === index && selectedParticipant === f.creatorUUID),
     [index, figures, selectedParticipant]
@@ -16,7 +19,18 @@ export default function CanvasBoard() {
   useEffect(() => dispatch(getBoard()), []);
 
   // eslint-disable-next-line
-  useEffect(() => selectedParticipant || dispatch(setSelectedParticipant(userUUID)), [userUUID, selectedParticipant]);
+  useEffect(() => selectedParticipant || dispatch(setSelectedParticipant(profile.uuid)), [profile, selectedParticipant]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.removeAllListeners();
+      socket.on('figuresCU', (figure) => dispatch(setFigure(figure)));
+      socket.on('figureD', (figure) => dispatch(removeFigure(figure.uuid)));
+      socket.on('board', (board) => dispatch(setBoard(board)));
+      socket.on('canvas', (user) => dispatch(setCanvasIndex(user.currentCanvas)));
+    }
+    // eslint-disable-next-line
+  }, [socket]);
 
   return <WidgetEditor figures={filteredFigures} copiedFigure={copiedFigure} index={index} />;
 }
